8fbfaa9f7ec4874db4724b5a719c2aafc88db870
Biolexey <Biolexey@gmail.com>
2022-03-25 15:30:40 +0900

    3/25

--- lecture/chapter2/cardioidOval.py
+++ lecture/chapter2/cardioidOval.py
@@ -3,14 +3,16 @@ import circle
 
 def display(canvas, points):
     for i in range(1, len(points)):
-        j = (2*i) % len(points)
-        canvas.create_line(points[i], points[j])
+        r = ((points[i][0]-points[0][0])**2 + (points[i][1]-points[0][1])**2)**0.5
+        ul = (points[i][0] - r, points[i][1] - r)
+        lr = (points[i][0] + r, points[i][1] + r)
+        canvas.create_oval(ul, lr)
 
 def main():
     root = Tk()
     canvas = Canvas(root, width=circle.W, height=circle.H)
     canvas.pack()
-    points = circle.circle()
+    points = circle.circle((circle.W/2+70, circle.H/2), 100)
     circle.display(canvas, points)
     display(canvas, points)
     root.mainloop()
